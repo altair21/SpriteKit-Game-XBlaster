2701ecb59a0fbceb1f38624e7728978d613a5cd4
altair21 <zz40@qq.com>
2015-06-05 22:58:30 +0800

    chapter.7 challenges

--- XBlaster/Entity.swift
+++ XBlaster/Entity.swift
@@ -9,25 +9,39 @@
 import SpriteKit
 
 class Entity: SKSpriteNode {
+    
+    struct ColliderType {
+        static var Player: UInt32 = 1
+        static var Enemy: UInt32 = 2
+        static var Bullet: UInt32 = 4
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        [super.init(coder: aDecoder)]
+    }
+    
     var direction = CGPointZero
     var health = 100.0
     var maxHealth = 100.0
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
+
     init(position: CGPoint, texture: SKTexture) {
-        super.init(texture: texture, color: SKColor.whiteColor(), size: texture.size())
+        super.init(texture: texture, color: SKColor.whiteColor(),
+            size: texture.size())
         self.position = position
     }
-    
+
     class func generateTexture() -> SKTexture? {
-        //Overridden by subclasses
+        // Overridden by subclasses
         return nil
     }
-    
+
     func update(delta: NSTimeInterval) {
-        //Overridden by subclasses
+        // Overridden by subclasses
     }
+    
+    func collidedWith(body: SKPhysicsBody, contact: SKPhysicsContact) {
+        // Overridden by subsclasses to implement actions to be carried out when an entity
+        // collides with another entity e.g. PlayerShip or Bullet
+    }  
+    
 }
--- XBlaster/GameScene.swift
+++ XBlaster/GameScene.swift
@@ -8,93 +8,227 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+// The update method uses the GameState to work out what should be done during each update
+// loop
+enum GameState {
+    case GameRunning
+    case GameOver
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
     let playerLayerNode = SKNode()
     let hudLayerNode = SKNode()
+    let bulletLayerNode = SKNode()
+    var enemyLayerNode = SKNode()
     let playableRect: CGRect
     let hudHeight: CGFloat = 90
     let scoreLabel = SKLabelNode(fontNamed: "Edit Undo Line BRK")
-    var scoreFlashAction: SKAction!                                     //score改变动画
-    let healthBarString: NSString = "===================="              //血量text
-    let playerHealthLabel = SKLabelNode(fontNamed: "Arial")             //血量label
-    var playerShip: PlayerShip!                                         //玩家飞船
-    var deltaPoint = CGPointZero                                        //手指与飞船距离
+    var scoreFlashAction: SKAction!
+    let healthBarString: NSString = "===================="
+    let playerHealthLabel = SKLabelNode(fontNamed: "Arial")
+    var playerShip: PlayerShip!
+    var deltaPoint = CGPointZero
+    var previousTouchLocation = CGPointZero
+    var bulletInterval: NSTimeInterval = 0
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    var score = 0
+    var gameState = GameState.GameOver
     
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    let gameOverLabel = SKLabelNode(fontNamed: "Edit Undo Line BRK")
+    let tapScreenPulseAction = SKAction.repeatActionForever(SKAction.sequence([
+        SKAction.fadeOutWithDuration(1),
+        SKAction.fadeInWithDuration(1)
+        ]))
+    let tapScreenLabel = SKLabelNode(fontNamed: "Edit Undo Line BRK")
     
     override init(size: CGSize) {
-        let maxAspectRatio: CGFloat = 16.0 / 9.0    //iphone 5
+        // Calculate playable margin
+        let maxAspectRatio: CGFloat = 16.0/9.0 // iPhone 5"
         let maxAspectRatioWidth = size.height / maxAspectRatio
         let playableMargin = (size.width - maxAspectRatioWidth) / 2.0
-        playableRect = CGRect(x: playableMargin, y: 0, width: size.width - playableMargin / 2, height: size.height - hudHeight)
+        playableRect = CGRect(x: playableMargin, y: 0,
+            width: size.width - playableMargin/2,
+            height: size.height - hudHeight)
         
         super.init(size: size)
+        
+        // Setup the initial game state
+        gameState = .GameRunning
         
         setupSceneLayers()
         setUpUI()
         setupEntities()
     }
     
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToView(view: SKView) {
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        physicsWorld.contactDelegate = self
+    }
+    
     func setupSceneLayers() {
         playerLayerNode.zPosition = 50
         hudLayerNode.zPosition = 100
+        bulletLayerNode.zPosition = 25
+        enemyLayerNode.zPosition = 35
         
         addChild(playerLayerNode)
         addChild(hudLayerNode)
+        addChild(bulletLayerNode)
+        addChild(enemyLayerNode)
     }
     
     func setUpUI() {
-        let backgroundSize = CGSize(width: size.width, height: hudHeight)
+        let backgroundSize =
+        CGSize(width: size.width, height:hudHeight)
         let backgroundColor = SKColor.blackColor()
-        let hudBarBackground = SKSpriteNode(color: backgroundColor, size: backgroundSize)
-        hudBarBackground.position = CGPoint(x: 0, y: size.height - hudHeight)
+        let hudBarBackground =
+        SKSpriteNode(color: backgroundColor, size: backgroundSize)
+        hudBarBackground.position =
+            CGPoint(x:0, y: size.height - hudHeight)
         hudBarBackground.anchorPoint = CGPointZero
         hudLayerNode.addChild(hudBarBackground)
         
+        // 1
         scoreLabel.fontSize = 50
         scoreLabel.text = "Score: 0"
         scoreLabel.name = "scoreLabel"
+        // 2
         scoreLabel.verticalAlignmentMode = .Center
-        scoreLabel.position = CGPoint(x: size.width / 2, y: size.height - scoreLabel.frame.size.height + 3)
+        // 3
+        scoreLabel.position = CGPoint(
+            x: size.width / 2,
+            y: size.height - scoreLabel.frame.size.height + 3)
+        // 4
         hudLayerNode.addChild(scoreLabel)
         
         scoreFlashAction = SKAction.sequence([
             SKAction.scaleTo(1.5, duration: 0.1),
             SKAction.scaleTo(1.0, duration: 0.1)])
-        scoreLabel.runAction(SKAction.repeatAction(scoreFlashAction, count: 20))
+        scoreLabel.runAction(
+            SKAction.repeatAction(scoreFlashAction, count: 20))
         
-        let playerHealthBackgroundLabel = SKLabelNode(fontNamed: "Arial")
+        // 1
+        let playerHealthBackgroundLabel =
+        SKLabelNode(fontNamed: "Arial")
         playerHealthBackgroundLabel.name = "playerHealthBackground"
         playerHealthBackgroundLabel.fontColor = SKColor.darkGrayColor()
         playerHealthBackgroundLabel.fontSize = 50
         playerHealthBackgroundLabel.text = healthBarString as String
+        playerHealthBackgroundLabel.zPosition = 0
+        // 2
         playerHealthBackgroundLabel.horizontalAlignmentMode = .Left
         playerHealthBackgroundLabel.verticalAlignmentMode = .Top
-        playerHealthBackgroundLabel.position = CGPoint(x: CGRectGetMinX(playableRect), y: size.height - CGFloat(hudHeight) + playerHealthBackgroundLabel.frame.size.height)
+        playerHealthBackgroundLabel.position = CGPoint(
+            x: CGRectGetMinX(playableRect),
+            y: size.height - CGFloat(hudHeight) +
+                playerHealthBackgroundLabel.frame.size.height)
         hudLayerNode.addChild(playerHealthBackgroundLabel)
-        
+        // 3
         playerHealthLabel.name = "playerHealthLabel"
-        playerHealthLabel.fontSize = 50
         playerHealthLabel.fontColor = SKColor.greenColor()
-        playerHealthLabel.text = healthBarString.substringToIndex(20*75/100)
+        playerHealthLabel.fontSize = 50
+        playerHealthLabel.text =
+            healthBarString.substringToIndex(20*75/100)
+        playerHealthLabel.zPosition = 1
         playerHealthLabel.horizontalAlignmentMode = .Left
         playerHealthLabel.verticalAlignmentMode = .Top
-        playerHealthLabel.position = CGPoint(x: CGRectGetMinX(playableRect), y: size.height - CGFloat(hudHeight) + playerHealthLabel.frame.size.height)
+        playerHealthLabel.position = CGPoint(
+            x: CGRectGetMinX(playableRect),
+            y: size.height - CGFloat(hudHeight) +
+                playerHealthLabel.frame.size.height)
         hudLayerNode.addChild(playerHealthLabel)
+        
+        gameOverLabel.name = "gameOverLabel"
+        gameOverLabel.fontSize = 100
+        gameOverLabel.fontColor = SKColor.whiteColor()
+        gameOverLabel.horizontalAlignmentMode = .Center
+        gameOverLabel.verticalAlignmentMode = .Center
+        gameOverLabel.position = CGPointMake(size.width / 2,
+            size.height / 2)
+        gameOverLabel.text = "GAME OVER";
+        
+        tapScreenLabel.name = "tapScreen"
+        tapScreenLabel.fontSize = 22;
+        tapScreenLabel.fontColor = SKColor.whiteColor()
+        tapScreenLabel.horizontalAlignmentMode = .Center
+        tapScreenLabel.verticalAlignmentMode = .Center
+        tapScreenLabel.position = CGPointMake(size.width / 2,
+            size.height / 2 - 100)
+        tapScreenLabel.text = "Tap Screen To Restart"
+        
     }
     
     func setupEntities() {
-        playerShip = PlayerShip(entityPosition: CGPoint(x: size.width / 2, y: 100))
+        playerShip = PlayerShip(
+            entityPosition: CGPoint(x: size.width / 2, y: 100))
         playerLayerNode.addChild(playerShip)
+        
+        // Add some EnemyA entities to the scene
+        for _ in 0..<3 {
+            let enemy = EnemyA(entityPosition: CGPointMake(
+                CGFloat.random(min: playableRect.origin.x, max: playableRect.size.width),
+                playableRect.size.height), playableRect: playableRect)
+            
+            // Set the initialWaypoint for the enemy to a random position within the playableRect
+            let initialWaypoint = CGPointMake(CGFloat.random(min: playableRect.origin.x, max: playableRect.size.width),
+                CGFloat.random(min: 0, max: playableRect.size.height))
+            enemy.aiSteering.updateWaypoint(initialWaypoint)
+            enemyLayerNode.addChild(enemy)
+        }
+        
+        for _ in 0..<3 {
+            let enemy = EnemyB(entityPosition: CGPointMake(
+                CGFloat.random(min: playableRect.origin.x, max: playableRect.size.width),
+                playableRect.size.height), playableRect: playableRect)
+            let initialWaypoint = CGPointMake(CGFloat.random(min: playableRect.origin.x, max: playableRect.width),
+                CGFloat.random(min: playableRect.height / 2, max: playableRect.height))
+            enemy.aiSteering.updateWaypoint(initialWaypoint)
+            enemyLayerNode.addChild(enemy)
+        }
+    }
+    
+    func increaseScoreBy(increment: Int) {
+        score += increment
+        scoreLabel.text = "Score: \(score)"
+        scoreLabel.removeAllActions()
+        scoreLabel.runAction(scoreFlashAction)
+    }
+    
+    func restartGame() {
+        // Reset the state of the game
+        gameState = .GameRunning
+        
+        // Setup the entities and reset the score
+        setupEntities()
+        score = 0
+        scoreLabel.text = "Score: 0"
+        
+        // Reset the players health and position
+        playerShip.health = playerShip.maxHealth
+        playerShip.position = CGPointMake(size.width / 2, 100)
+        
+        // Remove the game over HUD labels
+        gameOverLabel.removeFromParent()
+        tapScreenLabel.removeAllActions()
+        tapScreenLabel.removeFromParent()
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if gameState == .GameOver {
+            restartGame()
+        }
     }
     
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         let touch = (touches as NSSet).anyObject() as! UITouch
         let currentPoint = touch.locationInNode(self)
-        let previousTouchLocation = touch.previousLocationInNode(self)
+        previousTouchLocation = touch.previousLocationInNode(self)
         deltaPoint = currentPoint - previousTouchLocation
     }
     
@@ -107,11 +241,106 @@ class GameScene: SKScene {
     }
     
     override func update(currentTime: NSTimeInterval) {
-        var newPoint: CGPoint = playerShip.position + deltaPoint
-        newPoint.x.clamp(CGRectGetMinX(playableRect), CGRectGetMaxX(playableRect))
-        newPoint.y.clamp(CGRectGetMinY(playableRect), CGRectGetMaxY(playableRect))
+        // 1
+        var newPoint:CGPoint = playerShip.position + deltaPoint
+        // 2
+        newPoint.x.clamp(
+            CGRectGetMinX(playableRect), CGRectGetMaxX(playableRect))
+        newPoint.y.clamp(
+            CGRectGetMinY(playableRect),CGRectGetMaxY(playableRect))
+        // 3
         playerShip.position = newPoint
         deltaPoint = CGPointZero
+        
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        
+        switch gameState {
+        case (.GameRunning):
+            bulletInterval += dt
+            if bulletInterval > 0.15 {
+                bulletInterval = 0
+                
+                // 1: Create Bullet
+                let bullet = Bullet(entityPosition: playerShip.position)
+                
+                // 2: Add to scene
+                bulletLayerNode.addChild(bullet)
+                
+                // 3: Sequence: Move up screen, remove from parent
+                bullet.runAction(SKAction.sequence([
+                    SKAction.moveByX(0, y: size.height, duration: 1),
+                    SKAction.removeFromParent()
+                    ]))
+            }
+            
+            // Loop through all enemy nodes and run their update method.
+            // This causes them to update their position based on their currentWaypoint and position
+            for node in enemyLayerNode.children {
+                let enemy = node as! Enemy
+                enemy.update(self.dt)
+            }
+            
+            // Update the players health label to be the right length based on the players health and also
+            // update the color so that the closer to 0 it gets the more red it becomes
+            playerHealthLabel.fontColor = SKColor(red: CGFloat(2.0 * (1 - playerShip.health / 100)),
+                green: CGFloat(2.0 * playerShip.health / 100),
+                blue: 0,
+                alpha: 1)
+            
+            // Calculate the length of the players health bar.
+            let healthBarLength = Double(healthBarString.length) * playerShip.health / 100.0
+            playerHealthLabel.text = healthBarString.substringToIndex(Int(healthBarLength))
+            
+            // If the player health reaches 0 then change the game state.
+            if playerShip.health <= 0 {
+                gameState = .GameOver
+            }
+            
+        case (.GameOver):
+            
+            // When the game is over remove all the entities from the scene and add the game over labels
+            if !(gameOverLabel.parent != nil) {
+                bulletLayerNode.removeAllChildren()
+                enemyLayerNode.removeAllChildren()
+                playerShip.removeFromParent()
+                hudLayerNode.addChild(gameOverLabel)
+                hudLayerNode.addChild(tapScreenLabel)
+                tapScreenLabel.runAction(tapScreenPulseAction)
+            }
+            
+            // Set a random color for the game over label
+            gameOverLabel.fontColor = SKColor(red: CGFloat(drand48()),
+                green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+            
+        default:
+            println("UNKNWON GAME STATE")
+        }
+        
+    }
+    
+    // This method is called by the physics engine when two physics body collide
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+        // Check to see if Body A is an enamy ship and if so call collided with
+        if let enemyNode = contact.bodyA.node {
+            if enemyNode.name == "enemy" {
+                let enemy = enemyNode as! Entity
+                enemy.collidedWith(contact.bodyA, contact: contact)
+            }
+        }
+        
+        // ...and now check to see if Body B is the player ship/bullet
+        if let playerNode = contact.bodyB.node {
+            if playerNode.name == "playerShip" || playerNode.name == "bullet" {
+                let player = playerNode as! Entity
+                player.collidedWith(contact.bodyA, contact: contact)
+            }
+        }
     }
     
 }
